9d4314f102e9bd233c0eaed9d68e8b33a03f02b6
vito <chenhonglinchl@aliyun.com>
2019-09-20 18:35:34 +0800

    add files

---
+++ mbUartR.v
@@ -0,0 +1,9 @@
+// COMMIT: modbus串口服务模块
+// DSC:
+// 	接收总线上的数据，发出数据和数据有效标志
+
+module mbUartR(
+    port_list
+);
+    
+endmodule
---
+++ mbUartT.v
@@ -0,0 +1,9 @@
+// COMMIT: modbus串口服务模块
+// DSC:
+// 	接收数据和数据有效标志，通过串口发出，返回发送完成标志
+
+module mbUartT(
+    port_list
+);
+    
+endmodule
